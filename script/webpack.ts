f52fea60bff8904b329576c6e2b0bc4375d030ff
Alex Xu <Da.Alexxu@gmail.com>
2025-07-16 20:34:10 +1200

    Update: Configuration to typescript

---
+++ script/webpack.ts
@@ -0,0 +1,22 @@
+import { resolve } from "path";
+import { Configuration } from "webpack";
+
+const configuration: Configuration = {
+	mode: "production",
+	entry: "./src/main.ts",
+	output: {
+		filename: "bundle.js",
+		path: resolve("./public/"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: resolve("./node_modules/"),
+			},
+		],
+	},
+};
+
+export default configuration;
